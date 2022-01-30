bbb5178ea4d4129afef0df2b3899248813e7a84b
boris <wikiboris@gmail.com>
2022-01-30 16:40:37 +0100

    add prettier, eslint, tailwind, styled-components, prettier

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div>Initial State</div>;
-}
-
-export default App;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Component = styled.div``;
+
+export const App = () => {
+  return <Component className='text-blue-500'>Hello World</Component>;
+};
